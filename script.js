213ccda9e8673f79678722212667e821344c331e
Tanner Norton <tanneranorton23@gmail.com>
2018-02-15 16:03:37 -0700

    almost done

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 $(document).ready(function() {
     $("#nbaSubmit").click(function(e) {
     e.preventDefault();
-    var value = $("#nbaInput").val();
-    var myurl= "http://api.football-data.org/v1/fixtures?timeFrame=n2";
+    //var value = $("#nbaInput").val();
+    var myurl= "http://api.icndb.com/jokes/random/";
 
     $.ajax({
-        headers:{'X-Auth-Token':'3ea2ae4780b94241b0be5507865d1ae7'},
         url : myurl,
         dataType : "json",
-        type: 'GET',
+       
 
         success : function(json) {
             var results = "";
+
+            results += "<h2>" +  json.value.joke + "</h2>";
         $("#nbaResults").html(results);
         
+
         console.log(json);
-        }
-    });
-    });
-});
+        }//success close
+    });//ajax close
+    });//click close
+});//ready close
